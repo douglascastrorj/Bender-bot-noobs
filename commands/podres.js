61a013b0bd8c3de30ec770530d34ed97b2f9a9a4
Douglas <douglascastrorj@gmail.com>
2021-12-17 14:34:58 +0000

    comandos novos

---
+++ commands/podres.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+
+module.exports = { 
+  run: async ({client, message, args}) => {
+
+      const voiceChannel = message.member.voice.channel;
+      
+      voiceChannel.join().then(connection => {
+          const dispatcher = connection.play('./audio/podres.mp3')
+          dispatcher.on('end', end => voiceChannel.leave());
+      }).catch(err => console.log(err))
+    },
+  type: 'audio'
+  // }
+}
