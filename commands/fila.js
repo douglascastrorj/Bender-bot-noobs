29df3d0171a5607f7b2af66a27394e50880fd8ce
Augusto Rodrigues De Santana Neto <51301642+augustorsn@users.noreply.github.com>
2022-01-16 18:51:16 +0000

    feat: add fila mp3

---
+++ commands/fila.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+
+module.exports = { 
+  run: async ({client, message, args}) => {
+
+    const fileName = './audio/bananinhaclash.mp3';
+
+    // if (!client.isLocked() && message.content === 'Gotcha Bitch') {
+    //   client.lock()
+      const voiceChannel = message.member.voice.channel;
+      // console.log(voiceChannel)
+      voiceChannel.join().then(connection => {
+          const dispatcher = connection.play('./audio/fila.mp3')
+          dispatcher.on('end', end => voiceChannel.leave());
+      }).catch(err => console.log(err))
+      // client.unlock()
+  },
+  type: 'audio'
+  // }
+}
